040320717416a79f1048762abfab7b430c8ec466
JSY <jsyoo18@naver.com>
2026-03-04 09:59:28 +0900

    [REFACTOR]

--- src/main/java/com/careertalk/analysis/portfolio/service/PortfolioAnalysisService.java
+++ src/main/java/com/careertalk/analysis/portfolio/service/PortfolioAnalysisService.java
@@ -31,7 +31,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
