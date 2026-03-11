52cc71387c06d3edc7253300272b9b7d2abc211a
JSY <jsyoo18@naver.com>
2026-03-11 17:16:02 +0900

    [FIX] key값 버그 수정

--- src/main/java/com/careertalk/file/s3/S3Config.java
+++ src/main/java/com/careertalk/file/s3/S3Config.java
@@ -11,10 +11,10 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-    @Value("${AWS_ACCESS_KEY}")
+    @Value("${AWS_ACCESS_KEY_ID}")
     private String accessKey;
 
-    @Value("${AWS_SECRET_KEY}")
+    @Value("${AWS_SECRET_ACCESS_KEY}")
     private String secretKey;
 
     @Value("${aws.s3.region}")
